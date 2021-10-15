1f9d1b5ebf606bc21fbbea38d21c0a2516e68b17
AG00051917 <alejandro.guerrero@icumed.com>
2021-10-15 13:47:14 -0600

    FINAL

--- utils/database.py
+++ utils/database.py
@@ -1,54 +1,42 @@
-#books = []
-books_file = 'books.txt'
+from .database_connection import DatabaseConnection
+"""
+[{
+'name' : "",
+'author', "",
+'read' : True
+}]"""
 
 def create_book_table():
-    with open(books_file, 'w'):
-        pass # File exists
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 def add_book(name, author):
-    # books.append({'name': name, 'author': author, 'read': False})
-    with open(books_file,'a') as file:
-        file.write(f'{name}, {author},0\n')
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO books VALUES(?,?,0)', (name, author))
 
 
 def get_all_books():
-    # return books
-    with open(books_file, 'r') as file:
-        lines = [line.strip().split(',') for line in file.readlines()]
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    return [
-           {'name': line[0], 'author': line[1], 'read': line[2]}
-           for line in lines
-    ]
+        cursor.execute('SELECT * FROM books')
+        books = [{'name': row[0], 'author':row[1], 'read': row[2]} for row in cursor.fetchall()]
+
+    return books
 
 
 def mark_books_as_read(name):
-    # for book in books:
-    #    if book['name'] == name:
-    #       book['read'] = True
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = '1'
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
+        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
-def _save_all_books(books):
-   with open(books_file, 'w') as file:
-       for book in books:
-           file.write(f"{book['name']},{book['author']},{book['read']}\n")
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name]
-    _save_all_books(books)
-#second option with list
-#global books
-#books = [book for book in books if book['name'] != name]
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-#first option with dor
-#def prompt_delete_book(name):
- #   for book in books:
-  #      if book['name'] == name:
-    #        books.remove(book)
+        cursor.execute('DELETE FROM books WHERE name=?', (name,))
